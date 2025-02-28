6afda24728e46e0e0c6b4ef6501dc5989143744e
YunJOoo <qkdy1935@naver.com>
2025-02-28 17:54:58 +0900

    250228

--- scss-project/script.js
+++ scss-project/script.js
@@ -1,38 +1,89 @@
-/*  slick slider */
+// Gototop Event
+// 스크롤 이벤트
+window.addEventListener("scroll", () => {
+  const gototop = document.querySelector(".gototop");
+  const header = document.querySelector("header");
+  let scrollY = window.scrollY;
 
+  if (scrollY > 50) {
+    gototop.classList.add("active");
+    header.classList.add("active");
+  } else {
+    gototop.classList.remove("active");
+    header.classList.remove("active");
+  }
+});
+
+// Trigger Event
+const trigger = document.querySelector(".trigger");
+trigger.addEventListener("click", function () {
+  const gnb = document.querySelector(".gnb");
+  const menus = document.querySelectorAll(".menu a");
+
+  gnb.classList.toggle("active");
+  this.classList.toggle("active");
+
+  menus.forEach((menu) => {
+    menu.addEventListener("click", () => {
+      gnb.classList.remove("active");
+      trigger.classList.remove("active");
+    });
+  });
+});
+
+// Scroll Event /scrollTo
+$(".menu a , .gototop").click(function () {
+  $.scrollTo(this.hash || 0, 900);
+});
+
+// $(".gototop").click(function () {
+//   $.scrollTo(this.hash || 0, 900);
+// });
+
+// Slick Slider - 1
 $(".history-slider").slick({
   slidesToShow: 4,
   slidesToScroll: 2,
-  Infinity: false,
+  infinite: false,
   dots: true,
   speed: 300,
   responsive: [
     {
       breakpoint: 1024,
-      setings: {
+      settings: {
         slidesToShow: 3,
         slidesToScroll: 3,
-        Infinity: true,
+        infinite: true,
         dots: true,
       },
     },
     {
       breakpoint: 600,
-      setings: {
+      settings: {
         slidesToShow: 2,
         slidesToScroll: 2,
-        Infinity: true,
+        infinite: true,
         dots: true,
       },
     },
     {
       breakpoint: 480,
-      setings: {
+      settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
-        Infinity: true,
+        infinite: true,
         dots: true,
       },
     },
   ],
+});
+
+// Slick Slider - 2
+$(".project-photo").slick({
+  dots: true,
+  infinite: true,
+  fade: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  cssEase: "linear",
 });
